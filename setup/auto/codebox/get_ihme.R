0c6b9ca45b68d905d48e1c85c9745b38120939bd
Farshad Pourmalek <pourmalek_farshad@yahoo.com>
2021-06-02 05:55:01 -0700

    Update get_ihme.R

--- setup/auto/codebox/get_ihme.R
+++ setup/auto/codebox/get_ihme.R
@@ -12,7 +12,7 @@ page <- read_html("http://www.healthdata.org/covid/data-downloads")
 page %>%
   html_nodes("a") %>%       # find all links
   html_attr("href") %>%     # get the url
-  str_subset("\\.zip") %>% # find those that end in .zip
+  str_subset("\\.zip") %>%  # find those that end in .zip
   .[[1]]                    # return url of the first link
 
 
@@ -21,7 +21,7 @@ tmp[tmp %>%                 # return url of the latest link
       substr(1, 10) %>%
       as.Date() %>% which.max()]
 
-tmp %>%                    # return date of the latest link
+tmp %>%                     # return date of the latest link
   basename() %>%
   substr(1, 10) %>%
   as.Date() %>% max()
