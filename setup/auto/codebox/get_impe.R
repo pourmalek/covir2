7b85af3d615f968796bbb85733e871f7f67f4437
Farshad Pourmalek <pourmalek_farshad@yahoo.com>
2021-06-01 18:00:52 -0700

    Add files via upload

---
+++ setup/auto/codebox/get_impe.R
@@ -0,0 +1,21 @@
+
+# this is get_impe.R
+# to get the latest estimates file from IMPE
+# taken from:
+# https://stackoverflow.com/questions/31517121/using-r-to-scrape-the-link-address-of-a-downloadable-file-from-a-web-page
+
+# note: reads the first (earliest date) *.csv.zip file, not the wanted last (latest)
+
+library(rvest)
+library(stringr)
+library(xml2)
+
+page <- read_html("https://github.com/mrc-ide/global-lmic-reports/tree/master/data")
+
+page %>%
+  html_nodes("a") %>%       # find all links
+  html_attr("href") %>%     # get the url
+  str_subset("\\.csv.zip") %>% # find those that end in xlsx
+  .[[1]]                    # look at the first one
+
+
