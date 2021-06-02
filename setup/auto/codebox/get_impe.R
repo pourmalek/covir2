d22994d2d215a2212a776715a7d8ff4e772e317f
Farshad Pourmalek <pourmalek_farshad@yahoo.com>
2021-06-02 05:51:06 -0700

    Update get_impe.R

--- setup/auto/codebox/get_impe.R
+++ setup/auto/codebox/get_impe.R
@@ -12,14 +12,16 @@ page <- read_html("https://github.com/mrc-ide/global-lmic-reports/tree/master/da
 page %>%
   html_nodes("a") %>%       # find all links
   html_attr("href") %>%     # get the url
-  str_subset("\\.csv.zip") -> tmp # find those that end in .csv.zip
+  str_subset("\\.csv.zip") %>% # find those that end in .csv.zip
+  .[[1]]                    # return url of the first link
 
-tmp[tmp %>%                 # find url of the latest link
+
+tmp[tmp %>%                 # return url of the latest link
       basename() %>%
       substr(1, 10) %>%
       as.Date() %>% which.max()]
 
-tmp %>%                     # find date of the latest link
+tmp %>%                    # return date of the latest link
   basename() %>%
   substr(1, 10) %>%
   as.Date() %>% max()
